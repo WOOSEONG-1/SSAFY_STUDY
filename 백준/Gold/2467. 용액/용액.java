0541ed64ed076bdf45f2bb3d30aa49c913ca4876
WOOSEONG-1 <156279287+WOOSEONG-1@users.noreply.github.com>
2024-03-05 11:11:42 +0900

    [Gold V] Title: 용액, Time: 280 ms, Memory: 31300 KB -BaekjoonHub

---
+++ 백준/Gold/2467. 용액/용액.java
@@ -0,0 +1,48 @@
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.util.StringTokenizer;
+
+public class Main {
+
+	static int n;
+
+	public static void main(String[] args) throws IOException {
+		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+		StringBuffer sb = new StringBuffer();
+
+		n = Integer.parseInt(br.readLine());
+		StringTokenizer stk = new StringTokenizer(br.readLine());
+		int[] arr = new int[n];
+		for (int i = 0; i < n; i++) {
+			arr[i] = Integer.parseInt(stk.nextToken());
+
+		}
+		arr = two_point(arr);
+		sb.append(arr[0]).append(" ").append(arr[1]);
+		System.out.print(sb);
+	}
+
+	static int[] two_point(int[] arr) {
+		int left = 0;
+		int right = n-1;
+		int[] ans = new int[2];
+		int minv = Integer.MAX_VALUE;
+		// 갱신되면 right + 1, 갱신안되면 left + 1
+		while (left < right) {
+			
+			if ( minv > Math.abs(arr[left] + arr[right] )) {
+				minv = Math.abs(arr[left] + arr[right]);
+				ans[0] = arr[left];
+				ans[1] = arr[right];
+			}
+			if ( arr[right] + arr[left] >= 0 )
+				right--;
+			else
+				left++;
+
+		}
+
+		return ans;
+	}
+}
